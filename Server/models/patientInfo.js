14d1eba9feba267bb8ac2f42a7f6e6a13bc1cf0f
Phil Swartz <pswartz.sju@gmail.com>
2020-01-07 21:34:49 -0500

    updated models and controllers

---
+++ Server/models/patientInfo.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Patient Info Schema
+const PatientInfo = new Schema ({
+    email: {
+        type: String
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    dateofBirth: {
+        type: Date
+    },
+    ethnicity: {
+        type: String
+    },
+    weight: {
+        type: Number
+    },
+    height: {
+        type: Number
+    },
+    address: {
+        type: String
+    },
+    phoneNumber: {
+        type: String
+    },
+    disability: {
+        type: Boolean
+    },
+    tobaccoUse: {
+        type: Boolean
+    },
+    medications: {
+        type: String
+    },
+    doseage: {
+        type: Number
+    },
+    mrn: {
+        type: Number
+    },
+    heartRate: {
+        type: Number
+    },
+    bloodSugar: {
+        type: Number
+    },
+    bloodPressure: {
+        type: Number
+    },
+    moodSentiment: {
+        type: Number
+    },
+    date: {
+        type: new Date()
+    }
+
+})
+
+const patientInfo = mongoose.model("patientInfo", PatientInfo);
+
+module.exports = patientInfo;
+
