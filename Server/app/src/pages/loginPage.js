6fc4863600c50320e8165b133d2406fda7821b5c
Sam Levy <slevy239@gmail.com>
2020-01-07 23:21:07 -0500

    Dom's Commit

---
+++ Server/app/src/pages/loginPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class firstTime extends Component {
+    state = {
+        loginInfo: []
+    }
+}
+
+componentDidMount() {
+    
+}
+
+render() {
+    return (
+        
+    )
+}
