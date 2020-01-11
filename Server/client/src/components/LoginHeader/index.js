b4fb1984edd003f3dc40cb67af772ffb538bd8ae
Phil Swartz <pswartz.sju@gmail.com>
2020-01-11 12:26:05 -0500

    updated login page with routes and styling

--- Server/client/src/components/LoginHeader/index.js
+++ Server/client/src/components/LoginHeader/index.js
@@ -6,7 +6,7 @@ const LoginHeader = props => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light text-center">
             
-            <p className="p-2 m-0 headerTitle"><i class="fa fa-heartbeat 2x"></i>PatientFirst</p>
+            <p className="p-2 m-0 headerTitle"><i className="fa fa-heartbeat 2x"></i>PatientFirst</p>
             <div>
                 
             </div>
--- Server/client/src/pages/LoginPage.js
+++ Server/client/src/pages/LoginPage.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 import LoginHeader from '../components/LoginHeader';
 import LoginForm from '../components/LoginForm';
 
-
 class Login extends Component {
     constructor(props) {
         super(props);
@@ -14,7 +13,6 @@ class Login extends Component {
             password: ""
         }
     }
-
 
     componentDidMount = () => {
 
@@ -28,23 +26,17 @@ class Login extends Component {
         console.log(value);
     }
 
-
-
     render = () => {
         return (
-       
-                <div>
+            <div>
                 <Link
                     to="/Login"
                     className={window.location.pathname === "/Login"}
                 >
                 </Link>
-                
                 <LoginHeader />
                 <LoginForm />
             </div>
-            
-            
         )
     }
 }
