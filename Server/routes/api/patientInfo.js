919151a17f92d105f25dfb63eb9e79e1ac2d6d29
JackRyan1989 <jack.jackryan@gmail.com>
2020-01-12 10:23:43 -0500

    Implemented change to controller and to routes. Need to test.

--- Server/routes/api/patientInfo.js
+++ Server/routes/api/patientInfo.js
@@ -2,8 +2,21 @@ const router = require('express').Router();
 const controller = require('../../controllers/controller.js');
 
 // Matches with "/api/patient"
+// What do we want to do?
+// 1. Grab all user info
+// 2. Send over user data initially
+// 3. Update user data later
+// 4. Potentially delete the whole user
 router.route("/")
-     .get(controller.findAllData)
-    // .post(bookController.create);
+    .get(controller.findAllData)
+    .post(controller.createData);
 
+router.route('/update')
+    .post(updateFields)
+    .post(updateArrayFields);
+
+router.route('/removeData')
+    .get(controller.deletePatient)   
+    
+    
 module.exports = router;
