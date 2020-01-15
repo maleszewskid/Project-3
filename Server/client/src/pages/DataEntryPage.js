ff13b9d70460911a9d58fd906e300fb20a951cbb
Phil Swartz <pswartz.sju@gmail.com>
2020-01-14 22:11:52 -0500

    updated dataentry page, need to add to mongo db via api call

--- Server/client/src/pages/DataEntryPage.js
+++ Server/client/src/pages/DataEntryPage.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import EntryBody from '../components/EntryBody';
+import API from '../utils/API';
 
-export default class EntryBody extends Component {
+class DataEntry extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        systolic: "",
-        diastolic: "",
-        pulse: "",
-        glucose: "",
-        weight: "",
+        DataInputs: {}
       };
     }
 
-    handleChange = event => {
+    handleInputChange = event => {
+        // Getting the value and name of the input which triggered the change
+        const { name, value } = event.target;
+        // Updating the input’s state
         this.setState({
-          [event.target.id]: event.target.value
+            [name]: value
         });
-      }
+    };
 
+    // event handler for onclick of submit button in login form
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({
+            DataInputs: {
+            systolic: this.state.systolic,
+            diastolic: this.state.diastolic,
+            pulse: this.state.pulse,
+            glucose: this.state.glucose,
+            weight: this.state.weight,
+        }
+
+        })
+
+        // Need to send this to mongoDB via method called addPatientData
+        API.Login(this.state.DataInputs)
+            // here -> redirect the user to the landing page
+            .then(res => {
+                // Need to have this redirect to the main page:
+                if (res.status === 200) {
+                    // Set the redirect route:
+                    this.setState({
+                        redirectTo: '/Landing',
+                        loggedIn: true,
+                        username: res.data.username
+                    })
+                }
+            })
+            .catch(err => console.log(err)
+        )
+    }
+
+    
 
     render = () => {
-        const username = this.props.location.state;
+        console.log(this.state.DataInputs);
         return (
         <div>
             <Link
@@ -30,12 +63,14 @@ export default class EntryBody extends Component {
                 className={window.location.pathname === '/DataEntry'}
             >
             </Link>
-            <EntryBody onChange={this.handleChange} user={username} />
+            <EntryBody onChange={this.handleInputChange}
+                       onClick={this.handleSubmit} 
+                       data={this.state.DataInputs}/>
 
         </div>
         )
     }
 
-
+}
 
 export default DataEntry;
