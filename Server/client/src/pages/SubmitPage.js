65229574be455a3e3e81a1ae17f45ce31358d3eb
Dominick <Domdominick@hotmail.com>
2020-01-18 15:25:46 -0500

    pdf work in progress

--- Server/client/src/pages/SubmitPage.js
+++ Server/client/src/pages/SubmitPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from '../components/Header/Header';
 import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
 import PDF from "../components/createPDF/index2"
+import { LineChart, Chart } from 'react-chartkick'
+import 'chart.js'
 
 class SubmitToDoctor extends Component {
 //     constructor(props) {
@@ -20,9 +22,27 @@ class SubmitToDoctor extends Component {
 //         console.log(username);
 //     }
 
-    render = () => {
-        // console.log(this.props);
-        // const { username } = this.state;
+render = () => {
+    const { firstName,
+        lastName,
+        medications,
+        doseage,
+        heartRate,
+        bloodSugar,
+        systolicBloodPressure,
+        diastolicBloodPressure,
+        moodSentiment,
+        data,
+        dateOfBirth,
+        ethnicity,
+        weight,
+        height,
+        address,
+        phoneNumber,
+        disability,
+        tobaccoUse,
+        mrn
+    } = this.state.data;
                 return (
                 <>
                     {/* <Header user={username} /> */}
@@ -33,6 +53,16 @@ class SubmitToDoctor extends Component {
                         <PDFViewer>
                         <PDF />
                         </PDFViewer>
+                        </div>
+                        <div>
+                        Chart 1:  
+                        <LineChart xtitle='Date' ytitle='Blood Pressure' data={{ "2017-05-13": systolicBloodPressure, "2017-05-14": diastolicBloodPressure }} />
+                        Chart 2:
+                        <LineChart xtitle='Date' ytitle='Pulse' data={{ "2017-05-13": heartRate}} />
+                        Chart 3:
+                        <LineChart xtitle='Date' ytitle='Blood Glucose' data={{ "2017-05-13": bloodSugar }} />
+                        Chart 4:
+                        <LineChart xtitle='Date' ytitle='Weight | BMI' data={{ "2017-05-13": weight, "2017-05-13" : (snapshot.val().weight * 703) / (snapshot.val().height * snapshot.val().height) }} />
                         </div>
                 </>
                 )
