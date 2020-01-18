d75a1ac510b8ac1638d992ad76c1b9fbcfd2cbbe
Dominick <Domdominick@hotmail.com>
2020-01-18 15:09:49 -0500

    pdf work in progress

--- Server/client/src/pages/SubmitPage.js
+++ Server/client/src/pages/SubmitPage.js
@@ -1,34 +1,39 @@
 import React, { Component } from 'react';
 import Header from '../components/Header/Header';
-
+import { PDFDownloadLink, PDFviewer } from '@react-pdf/renderer';
+import PDF from "../components/createPDF/index2"
 
 class SubmitToDoctor extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            placeholder: ''
-        }
-    }
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             username: '',
+//             placeholder: ''
+//         }
+//     }
 
-    componentDidMount() {
-        this.setState({
-            username: this.props.location.state
-        });
-        const { username } = this.props.location.state;
-        console.log(username);
-    }
+//     componentDidMount() {
+//         this.setState({
+//             username: this.props.location.state
+//         });
+//         const { username } = this.props.location.state;
+//         console.log(username);
+//     }
 
     render = () => {
-        console.log(this.props);
-        const { username } = this.state;
+        // console.log(this.props);
+        // const { username } = this.state;
                 return (
                 <>
-                    <Header user={username} />
+                    {/* <Header user={username} /> */}
                     <div>
                         This is submit to Doctor Page
-                        
                     </div>
+                    <div>
+                        <PDFviewer>
+                        <PDF />
+                        </PDFviewer>
+                        </div>
                 </>
                 )
         }
