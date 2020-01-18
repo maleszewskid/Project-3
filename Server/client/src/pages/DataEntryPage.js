0066ef78667b05089abd1d8ab743cfedf7bc78d6
JackRyan1989 <jack.jackryan@gmail.com>
2020-01-18 14:05:19 -0500

    Data submission is happening, with some error handling. However, things are not going in as we want.

--- Server/client/src/pages/DataEntryPage.js
+++ Server/client/src/pages/DataEntryPage.js
@@ -8,7 +8,16 @@ class DataEntry extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: ''
+            username: '',
+            addDoseage: false,
+            bloodError: false,
+            moodError: false,
+            medError: false,
+            genError: false,
+            bloodSuccess: false,
+            moodSuccess: false,
+            medSuccess: false,
+            genSuccess: false,
         };
     }
 
@@ -23,7 +32,12 @@ class DataEntry extends Component {
         const { name, value } = event.target;
         // Updating the input’s state
         this.setState({
-            [name]: value
+            [name]: value,
+            addDoseage: false,
+            bloodSuccess: false,
+            moodSuccess: false,
+            medSuccess: false,
+            genSuccess: false
         });
     };
 
@@ -39,11 +53,19 @@ class DataEntry extends Component {
             diastolicBloodPressure
         };
         // Need to send this to mongoDB via method called addPatientData
-        API.submitPatientData({data})
-            .then(res => console.log(res))
+        API.submitPatientData({ data })
+            .then(data => {
+                if (data) {
+                    this.setState({
+                        bloodSuccess: true
+                    })
+                }
+            })
             .catch(err => {
                 if (err) {
-                    console.log(err)
+                    this.setState({
+                        bloodError: true
+                    })
                 }
             })
     }
@@ -57,11 +79,19 @@ class DataEntry extends Component {
             journalEntry
         };
         // Need to send this to mongoDB via method called addPatientData
-        API.submitPatientData({data})
-            .then(res => console.log(res))
+        API.submitPatientData({ data })
+            .then(data => {
+                if (data) {
+                    this.setState({
+                        moodSuccess: true
+                    })
+                }
+            })
             .catch(err => {
                 if (err) {
-                    console.log(err)
+                    this.setState({
+                        moodError: true
+                    })
                 }
             })
     }
@@ -74,41 +104,71 @@ class DataEntry extends Component {
         if (this.state.medication1 && this.state.doseage1) {
             medArray.push(this.state.medication1);
             doseArray.push(this.state.doseage1);
+        } else if (this.state.medication1 && !this.state.doseage1) {
+            this.setState({
+                addDoseage: true
+            })
         }
         if (this.state.medication2 && this.state.doseage2) {
             medArray.push(this.state.medication2);
             doseArray.push(this.state.doseage2);
+        } else if (this.state.medication2 && !this.state.doseage2) {
+            this.setState({
+                addDoseage: true
+            })
         }
         if (this.state.medication3 && this.state.doseage3) {
             medArray.push(this.state.medication3);
             doseArray.push(this.state.doseage3);
+        } else if (this.state.medication3 && !this.state.doseage3) {
+            this.setState({
+                addDoseage: true
+            })
         }
         if (this.state.medication4 && this.state.doseage4) {
             medArray.push(this.state.medication4);
             doseArray.push(this.state.doseage4);
+        } else if (this.state.medication4 && !this.state.doseage4) {
+            this.setState({
+                addDoseage: true
+            })
         }
         if (this.state.medication5 && this.state.doseage5) {
             medArray.push(this.state.medication5);
             doseArray.push(this.state.doseage5);
+        } else if (this.state.medication5 && !this.state.doseage5) {
+            this.setState({
+                addDoseage: true
+            })
         }
         const data = {
             username: this.state.username,
             medications: medArray,
             doseage: doseArray
         };
-        API.submitPatientData({data})
-            .then(res => console.log(res))
-            .catch(err => {
-                if (err) {
-                    console.log(err)
-                }
-            })
+        if (!this.state.addDoseage) {
+            API.submitPatientData({ data })
+                .then(data => {
+                    if (data) {
+                        this.setState({
+                            medSuccess: true
+                        })
+                    }
+                })
+                .catch(err => {
+                    if (err) {
+                        this.setState({
+                            medError: true
+                        })
+                    }
+                })
+        }
     }
 
     // Event handler to submit general data:
     handleGeneralSubmit = event => {
         event.preventDefault();
-        const { username, feet, inches, weight, ethnicity, disability, tobaccoUse  } = this.state;
+        const { username, feet, inches, weight, ethnicity, disability, tobaccoUse } = this.state;
         const data = {
             username,
             height: [feet, inches],
@@ -117,11 +177,19 @@ class DataEntry extends Component {
             disability,
             tobaccoUse
         };
-        API.submitPatientData({data})
-            .then(res => console.log(res))
+        API.submitPatientData({ data })
+            .then(data => {
+                if (data) {
+                    this.setState({
+                        genSuccess: true
+                    })
+                }
+            })
             .catch(err => {
                 if (err) {
-                    console.log(err)
+                    this.setState({
+                        genError: true
+                    })
                 }
             })
     }
@@ -137,7 +205,16 @@ class DataEntry extends Component {
                     onMoodClick={this.handleMoodSubmit}
                     onMedsClick={this.handleMedsSubmit}
                     onGenClick={this.handleGeneralSubmit}
-                     />
+                    bloodError={this.state.bloodError}
+                    moodError={this.state.moodError}
+                    medError={this.state.medError}
+                    genError={this.state.genError}
+                    addDoseage={this.state.addDoseage}
+                    bloodSuccess={this.state.bloodSuccess}
+                    moodSuccess={this.state.moodSuccess}
+                    medSuccess={this.state.medSuccess}
+                    genSuccess={this.state.genSuccess}
+                />
             </>
         )
     }
