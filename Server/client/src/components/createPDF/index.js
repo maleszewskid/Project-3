1489f11c9928fcc2aeb5a4ce71591da07926fa70
Sam Levy <slevy239@gmail.com>
2020-01-23 23:32:03 -0500

    some pdf styling

--- Server/client/src/components/createPDF/index.js
+++ Server/client/src/components/createPDF/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Page, Text, Document } from '@react-pdf/renderer';
-import queue from 'queue'
+import { Page, Text, Document, View, Title, Author } from '@react-pdf/renderer';
+import Header from '../Header/Header';
 
-  // const renderQueue = queue({
-  //   autostart: true,
-  //   concurrency: 1
-  // })
+
+
 
 const ViewPDF = (props) => {
   const {
@@ -30,26 +28,32 @@ const ViewPDF = (props) => {
     mrn
   } = props.data;
   let height1 = (height[0] * 12) + height[1];
-  let personalData = lastName + ", " + firstName + " DOB: " + dateOfBirth + " MRN: " 
-  + mrn;
+  let personalData = lastName + ", " + firstName + " DOB: " + dateOfBirth + " MRN: "+ mrn;
 
 
-  //  let heart = "Blood Pressure | Systolic: " + systolicBloodPressure + " mm Hg | Diastolic: " + diastolicBloodPressure + " mm Hg (View Chart 1) | Pulse: " + heartRate + " bpm (View Chart 2)"
-  // let diabetic = "Blood Glucose: " + bloodSugar + " mg/dL (View Chart 3) | Weight: " + weight + " lbs | BMI: " + (weight * 703) / (height1 * height1) + " (View Chart 4)"
+  let heart = "Blood Pressure | Systolic: " + systolicBloodPressure + " mm Hg | Diastolic: " + diastolicBloodPressure + " mm Hg (View Chart 1) | Pulse: " + heartRate + " bpm (View Chart 2)\n"
+  let diabetic = "Blood Glucose: " + bloodSugar + " mg/dL (View Chart 3) | Weight: " + weight + " lbs | BMI: " + (weight * 703) / (height1 * height1) + " (View Chart 4)\n"
+
+
+
 
   return (
     <Document>
-      <Page>
-        <Text>
-          {personalData}
-          HI!!!!
-          {/* {diabetic} */}
-        </Text>
+      <Page size='A4' style={{ backgroundColor: 'black', color: 'white' }}>
+        <View style={{ color: 'white', textAlign: 'center', margin: 30 }}>
+          {/* <Header>Medical Data</Header> */}
+          {/* <Title>Don Quijote de la Mancha</Title> */}          
+          <Text>
+            {personalData}
+            {diabetic}
+            {heart}
+          </Text>
+        </View>
       </Page>
     </Document>
   )
 
-    // renderQueue.push(() => PDFPDF())
+  // renderQueue.push(() => PDFPDF())
 
 }
 
