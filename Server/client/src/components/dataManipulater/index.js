b31287e4be36b2de402963bf74adf6b1fd76c0d1
Dominick <Domdominick@hotmail.com>
2020-01-25 14:03:15 -0500

    chart to pdf

--- Server/client/src/components/dataManipulater/index.js
+++ Server/client/src/components/dataManipulater/index.js
@@ -1,4 +1,4 @@
-let y = document.getElementsByClassName("chartjs-size-monitor")
+let y = document.getElementsByClassName[0]("chartjs-size-monitor")
 y.onmouseover = function (){
 let x = document.getElementsByTagName("link").getAttribute("href");
 console.log(x)
