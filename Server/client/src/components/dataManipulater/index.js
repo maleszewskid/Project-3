b21f7ecb656bcbe09296d2e72e15b73b32d6e7fc
Dominick <Domdominick@hotmail.com>
2020-01-25 13:59:00 -0500

    chart to pdf

--- Server/client/src/components/dataManipulater/index.js
+++ Server/client/src/components/dataManipulater/index.js
@@ -1,6 +1,6 @@
-
-
-let x = document.getElementById("link")[0].getAttribute("href");
+let y = document.getElementById("chart-1")
+y.addEventListener("mouseover", function (){
+let x = document.getElementsByTagName("link").getAttribute("href");
 console.log(x)
-
+}
             
