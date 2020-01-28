ec8c6c95c5444bf8fa570cadde3a33c2e6f50545
Dominick Maleszewski <domdominick@hotmail.com>
2020-01-27 21:58:19 -0500

    build

---
+++ Server/client/build/precache-manifest.48c15638d344c0759223367f8086bff4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975b10518fd3f39e2daef3584330323b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62fe9aaa454c3587cd5a",
+    "url": "/static/css/main.2d4c27db.chunk.css"
+  },
+  {
+    "revision": "de6e895f080bc165ee34",
+    "url": "/static/js/2.1af9c080.chunk.js"
+  },
+  {
+    "revision": "a5d69720209cd277a69ab4988cf579b1",
+    "url": "/static/js/2.1af9c080.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62fe9aaa454c3587cd5a",
+    "url": "/static/js/main.2fc2939b.chunk.js"
+  },
+  {
+    "revision": "5536b1ffa5673e4eb964",
+    "url": "/static/js/runtime-main.d07f956b.js"
+  },
+  {
+    "revision": "3636ae17dac6e798d0867cae1293d591",
+    "url": "/static/media/depositphotos_158934956-stock-illustration-seamless-geometric-line-grid-vector.3636ae17.jpg"
+  }
+]);
