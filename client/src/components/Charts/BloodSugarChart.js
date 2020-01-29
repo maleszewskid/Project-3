5af1a0f5adf41e61b1502cba51cc491454cb9d27
Dominick <Domdominick@hotmail.com>
2020-01-28 20:17:14 -0500

    dataview

--- client/src/components/Charts/BloodSugarChart.js
+++ client/src/components/Charts/BloodSugarChart.js
@@ -5,7 +5,7 @@ import 'chart.js'
 const BloodSugarChart = props => {
     return (
         <>
-            <h2 className='mx-2' >{props.data[0].name}</h2>
+            <h2 className='mx-2' >Blood Sugar</h2>
             <LineChart className='p-2' data={props.data} download={true}/>
         </>
     )
